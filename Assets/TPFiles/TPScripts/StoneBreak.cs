d1d27d7d9afe39a534bc270723b142202cea7459
Alex McDowell <mmcdowell@student.neumont.edu>
2022-04-07 16:55:43 -0600

    Adding Breaking Rock via Gravity
    Changed so the rock breaks thanks to gravity rather than just deleting segments.

--- Assets/TPFiles/TPScripts/StoneBreak.cs
+++ Assets/TPFiles/TPScripts/StoneBreak.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class StoneBreak : MonoBehaviour
 {
-
     public GameObject[] RockPieces;
+    int pieceNextToBreak = -1;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Click");
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 100.0f) && hit.transform.tag == "Rock"){
+                if (pieceNextToBreak > RockPieces.Length)
+                {
+                    
+                    pieceNextToBreak += 0;
+                    Debug.Log(pieceNextToBreak);
+                }
+                else
+                {
+                    pieceNextToBreak++;
+                    BreakPiece();
+                    Debug.Log(pieceNextToBreak);
+                }
+            
+            }
+        }
+    }
+
+    public void BreakPiece()
+    {
+        if(pieceNextToBreak == RockPieces.Length)
+        {
+            Debug.Log("All Broken Off");
+        }
+        else
+        {
+            RockPieces[pieceNextToBreak].GetComponent<Rigidbody>().useGravity = true;
+            //Destroy(RockPieces[pieceNextToBreak]);
+            Debug.Log("Pieces Left: " + pieceNextToBreak);
+        }
     }
 }
