27622b78285ec1b9df7309f335048681de99b5aa
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-04-12 16:13:43 -0600

    Player no longer move/fall while loading
    OVRPlayerController is now disabled while game is loading, re-enabled when loading is done.

--- Assets/Scripts/NonVR/UIManagement/LoadManager.cs
+++ Assets/Scripts/NonVR/UIManagement/LoadManager.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class LoadManager : MonoBehaviour
 {
-
     public GameObject LoadingScreen;
     public GameObject LoadingRaptor;
+    public OVRPlayerController playerController;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        if (playerController != null) playerController.enabled = false;
         StartCoroutine(LoadingScreenCo());
     }
-
 
     IEnumerator LoadingScreenCo()
     {
         yield return new WaitForSeconds(5);
         LoadingRaptor.SetActive(false);
         LoadingScreen.SetActive(false);
+        if (playerController != null) playerController.enabled = true;
         yield return true;
     }
 }
