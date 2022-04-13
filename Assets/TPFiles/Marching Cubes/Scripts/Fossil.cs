88e290d1e52cafe78b02c65f409cb3feed7a77ab
Matthew Mason <matthewmason7@outlook.com>
2022-04-13 15:01:42 -0600

    fossil unearthing is detected

--- Assets/TPFiles/Marching Cubes/Scripts/Fossil.cs
+++ Assets/TPFiles/Marching Cubes/Scripts/Fossil.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class Fossil : MonoBehaviour
 {
     public bool wasFound = false;
+    public bool buried = true;
     public string name = null;
+
+    private bool startDigging = false;
+
+    //happens before the collisions
+    private void FixedUpdate()
+    {
+        if (startDigging)
+        {
+            if (!buried)
+            {
+                wasFound = true;
+                Debug.Log("Bruh it works");
+            }
+            if (!wasFound)
+            {
+                buried = false;
+            }
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-
+            if (!startDigging)
+            {
+                startDigging = true;
+            }
+            if (!wasFound)
+            {
+                buried = true;
+            }
         }
     }
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        if(collision.gameObject.tag == "Ground" && !wasFound)
         {
-
+            buried = true;
         }
     }
 }
