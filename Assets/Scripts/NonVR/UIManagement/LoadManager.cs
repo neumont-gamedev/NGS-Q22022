ddcf43c058e076cf03f17d374e8a3565a208081e
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-04-13 14:25:18 -0600

    Loading Screen bug fixed
    Player can no longer activate their shovel while Loading Screen is active

--- Assets/Scripts/NonVR/UIManagement/LoadManager.cs
+++ Assets/Scripts/NonVR/UIManagement/LoadManager.cs
@@ -7,11 +7,14 @@ public class LoadManager : MonoBehaviour
     public GameObject LoadingScreen;
     public GameObject LoadingRaptor;
     public OVRPlayerController playerController;
+    public ObjectEnabler enabler;
 
     void Awake()
     {
         playerController = GetComponentInParent<OVRPlayerController>();
+        if (enabler == null) enabler = GetComponentInParent<ObjectEnabler>();
         if (playerController != null) playerController.enabled = false;
+        if (enabler != null) enabler.enabled = false;
 
         StartCoroutine(LoadingScreenCo());
     }
@@ -22,6 +25,7 @@ public class LoadManager : MonoBehaviour
         LoadingRaptor.SetActive(false);
         LoadingScreen.SetActive(false);
         if (playerController != null) playerController.enabled = true;
+        if (enabler != null) enabler.enabled = true;
         yield return true;
     }
 }
