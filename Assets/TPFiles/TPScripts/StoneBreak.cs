72529448c2cf9536a50457ded40cadebed8913fd
Alex McDowell <mmcdowell@student.neumont.edu>
2022-04-14 13:11:57 -0600

    Trying to make a shader
    I failed

--- Assets/TPFiles/TPScripts/StoneBreak.cs
+++ Assets/TPFiles/TPScripts/StoneBreak.cs
@@ -8,21 +8,28 @@ public class StoneBreak : MonoBehaviour
     public GameObject breakParticle;
     public GameObject cBreakParticle;
     static int numofPiecesToBreak;
+    Renderer renderer;
     //public int numofPieces;
     //int curlength;
 
     public void BreakPiece()
     {
+
+        
         numofPiecesToBreak = RockPieces.Count;
+        
 
         int ranNum = Random.Range(0, RockPieces.Count);
 
         if (RockPieces[ranNum].GetComponent<Rock>().breakPoint > 0)
         {
+            RockPieces[ranNum].GetComponent<Renderer>().sharedMaterial.SetFloat("BlendEffect", .5f);
             RockPieces[ranNum].GetComponent<Rock>().breakPoint--;
+
         }
         else 
         {
+            RockPieces[ranNum].GetComponent<Renderer>().sharedMaterial.SetFloat("BlendEffect", 0f);
             RockPieces[ranNum].GetComponent<Rigidbody>().useGravity = true;
             Destroy(RockPieces[ranNum], 1.5f);
             RockPieces.RemoveAt(ranNum);
