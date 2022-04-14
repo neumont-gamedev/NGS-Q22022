b8a48f4f493ef1bfb457c286ea012c1f6cb5a0fd
CameronChildress <cameronchildress4@gmail.com>
2022-04-14 13:11:46 -0600

    VR Raycast Done

--- Assets/TPFiles/TPScripts/TestRayCast.cs
+++ Assets/TPFiles/TPScripts/TestRayCast.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class TestRayCast : MonoBehaviour
 {
+    public TestVRInput VRInput;
+
+    //public void OnPointerClick(PointerEventData eventData)
+    //{
+    //    if (VRInput.GetMouseButtonDown(0))
+    //    {
+    //        RaycastHit hit;
+    //        Ray ray = Camera.main.ScreenPointToRay(VRInput.mousePosition);
+    //        if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform.tag == "Rock")
+    //        {
+    //            Debug.Log("Rock Clicked");
+    //            hit.transform.gameObject.GetComponent<StoneBreak>().BreakPiece();
+    //        }
+    //    }
+    //}
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +31,10 @@ public class TestRayCast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (VRInput.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(VRInput.mousePosition);
             if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform.tag == "Rock")
             {
                 Debug.Log("Rock Clicked");
