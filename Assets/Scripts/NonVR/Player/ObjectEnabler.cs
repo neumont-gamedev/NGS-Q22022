e1675ec5069d9b1b9bd47ff0a36f6f355c89dd56
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-04-15 17:12:24 -0600

    Improved ObjectEnabler
    ObjectEnabler now allows an array of objects, cycled by the one button click. Created Stick Prefab, used for hitting UI Buttons from further away.

--- Assets/Scripts/NonVR/Player/ObjectEnabler.cs
+++ Assets/Scripts/NonVR/Player/ObjectEnabler.cs
@@ -5,18 +5,42 @@ using UnityEngine.InputSystem;
 
 public class ObjectEnabler : MonoBehaviour
 {
-    public GameObject obj;
+    public GameObject[] objects;
+    public GameObject controller;
     public OVRInput.Button button = OVRInput.Button.Three;
     bool pressedLastFrame = false;
 
-    void Start()
+    int counter = 0;
+
+    void Awake()
     {
-        obj.SetActive(false);
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(false);
+        }
+
+        if (controller == null) controller = GameObject.Find("RightControllerAnchor");
+        controller.SetActive(true);
     }
 
     void Update()
     {
-        if(OVRInput.Get(button) && !pressedLastFrame) obj.SetActive(!obj.activeInHierarchy);
+        if (OVRInput.Get(button) && !pressedLastFrame)
+        {
+            if (counter < objects.Length)
+            {
+                objects[counter].SetActive(true);
+                controller.SetActive(false);
+                if (counter-1 >= 0) objects[counter-1].SetActive(false);
+                counter++;
+            }
+            else
+            {
+                objects[counter-1].SetActive(false);
+                controller.SetActive(true);
+                counter = 0;
+            }
+        }
 
         pressedLastFrame = OVRInput.Get(button);
     }
