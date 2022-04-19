ecbee8130a53f1c81d3e9f5824851a22e42ccf1b
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-04-19 16:32:09 -0600

    Player can no longer dig too close to start position.

--- Assets/TPFiles/TPScripts/VRTerrainModifier.cs
+++ Assets/TPFiles/TPScripts/VRTerrainModifier.cs
@@ -17,12 +17,20 @@ public class VRTerrainModifier : MonoBehaviour
     [Range(0, Constants.NUMBER_MATERIALS - 1)]
     public int buildingMaterial = 0;
 
+    public int startingChunkX = 1;
+    public int startingChunkZ = -1;
+    public float chunkSize = 8f;
+
+    private string startingChunk = "Chunk_1|-1";
     private ChunkManager chunkManager;
+    private Vector3 startPos = Vector3.zero;
 
     void Start()
     {
         chunkManager = ChunkManager.Instance;
         digNoise = GetComponent<AudioSource>();
+        startingChunk = $"Chunk_{startingChunkX}|{startingChunkZ}";
+        startPos = GameObject.Find("StartPosition").transform.position;
         UpdateUI();
     }
 
@@ -35,6 +43,18 @@ public class VRTerrainModifier : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Ground")) return;
+
+        Debug.LogWarning($"Y: {collision.contacts[0].point.y}");
+        Debug.LogWarning($"Chunk Hit: {collision.gameObject.name}");
+        Debug.LogWarning($"Starting Chunk: {startingChunk}");
+
+        // Chunk_1|-1 position = 8, -8
+        float x = Mathf.Abs(collision.contacts[0].point.x - (startingChunkX * chunkSize));
+        float z = Mathf.Abs(collision.contacts[0].point.z - (startingChunkZ * chunkSize));
+        float distance = Mathf.Abs((startPos - collision.contacts[0].point).magnitude);
+
+        //if (x <= 4f && z <= 4f) return;
+        if (distance <= 4f) return;
 
         float modification = -modiferStrengh;
         var contacts = collision.contacts;
