8abdf40d0a6ba290fb829c1a10e13d52cb84d64c
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-04-21 16:12:43 -0600

    Fossils now always spawn within barrier and underground

--- Assets/TPFiles/Marching Cubes/Scripts/TerrainGenerator/NoiseManager.cs
+++ Assets/TPFiles/Marching Cubes/Scripts/TerrainGenerator/NoiseManager.cs
@@ -49,8 +49,8 @@ namespace MarchingCubes
 				public float appearValue;//1 to 0 value when the biome appears. Next biomePropertie.appear value is where this biome end
 		  }
 
-		  private int fossilDepth = -3;
-		  private int fossilBorders = 60;
+		  private int fossilDepth = -6;
+		  private int fossilBorders = 45;
 		  private Dictionary<string, KeyValuePair<float, float>> xzs = new Dictionary<string, KeyValuePair<float, float>>();
 
 		//resets world data and calls startup commands
