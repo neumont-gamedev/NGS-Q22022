789a6cb85110ce10a2ffc9507d74c2e05813410b
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-05-10 14:49:42 -0600

    Minor UI change, Build settings update

--- Assets/TPFiles/TPScripts/CleaningScripts/CleaningUIManager.cs
+++ Assets/TPFiles/TPScripts/CleaningScripts/CleaningUIManager.cs
@@ -15,6 +15,8 @@ public class CleaningUIManager : MonoBehaviour
     public Toggle Polishtoggle;
     public Text polishToggleText;
 
+    public GameObject putTogetherDisplay;
+
 
     public void RockBreakToggleChange()
     {
@@ -26,22 +28,24 @@ public class CleaningUIManager : MonoBehaviour
     {
         Polishtoggle.isOn = false;
         Cleantoggle.isOn = true;
+        putTogetherDisplay.SetActive(true);
     }
 
     public void CleanToggleTextChange(int cleanedBones, int maxBones)
     {
-        cToggleText.text = "Clean Bones - Bones Cleaned: " + cleanedBones + "/" + (maxBones + 1);
+        cToggleText.text = "Clean Bones. Cleaned: " + cleanedBones + "/" + (maxBones + 1);
     }
     
     public void PolishToggleTextChange(int polishedBones, int maxBones)
     {
-        polishToggleText.text = "Polish Bones - Bones Polished: " + polishedBones + "/" + (maxBones + 1);
+        polishToggleText.text = "Polish Bones. Polished: " + polishedBones + "/" + (maxBones + 1);
     }
 
     public void CombineToggleChange()
     {
         Polishtoggle.isOn = false;
         Combinetoggle.isOn = true;
+        putTogetherDisplay.SetActive(false);
     }
 
 
