4b3c934eac6b2b3802db25eacb3bedc399c6ffc3
CameronChildress <cameronchildress4@gmail.com>
2022-05-12 14:47:53 -0600

    Identify Data started

--- Assets/TPFiles/TPScripts/CleaningScripts/Datas/DataHolder.cs
+++ Assets/TPFiles/TPScripts/CleaningScripts/Datas/DataHolder.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataHolder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public IdentifyData boneData;
+
+    public TMP_Text RightAnswersText;
+
     void Start()
     {
-        
+        RightAnswersText.text = boneData.FinalAnswer;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
