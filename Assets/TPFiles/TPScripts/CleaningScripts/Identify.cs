99c613235c368d3e80a4839a6e28e5c4146eb78f
CameronChildress <cameronchildress4@gmail.com>
2022-05-13 13:50:18 -0600

    Identify displays info

--- Assets/TPFiles/TPScripts/CleaningScripts/Identify.cs
+++ Assets/TPFiles/TPScripts/CleaningScripts/Identify.cs
@@ -15,26 +15,28 @@ public class Identify : MonoBehaviour
     public TMP_Text uPart;
     public TMP_Text uCreature;
 
-    public IdentifyData curData;
+    public DataHolder curData;
 
     public GameObject resultsPage;
- 
+
+    private void Start()
+    {
+        curData = FindObjectOfType<DataHolder>();
+    }
+
     public void CheckQuestion()
     {
-
-        curData = FindObjectOfType<IdentifyData>();
-
-        if(curData.Creature_Name.ToString() == "Trilobite")
+        if(curData.boneData.Creature_Name.ToString() == "Trilobite")
         {
-            uAnswer.text = "The fossil is the " + uPart + "of a " + uCreature;
+            uAnswer.text = "The fossil is the " + uPart.text + "of a " + uCreature.text;
         }
         else
         {
-            uAnswer.text = "The fossil is the " + uPart + "of a " + uCreature + " it has " + uMarkings + " marks. This creature " + uHappened;
+            uAnswer.text = "The fossil is the " + uPart.text + "of a " + uCreature.text + " it has " + uMarkings.text + " marks. This creature " + uHappened.text;
         }
       
 
-        rAnswer.text = curData.FinalAnswer;
+        rAnswer.text = curData.boneData.FinalAnswer;
         resultsPage.SetActive(true);
 
 
