851eb3a5779207d8e90f2fddc04082c77f3648f6
Garrick Kilpack <garrickkilpack@hotmail.com>
2022-05-13 16:58:44 -0600

    SFX applied where I had time

--- Assets/TPFiles/Scripts/UIManagement/InGameButton.cs
+++ Assets/TPFiles/Scripts/UIManagement/InGameButton.cs
@@ -3,67 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class InGameButton : MonoBehaviour
 {
     Button button;
+    AudioSource clickAudio;
 
     void Start()
     {
         button = GetComponent<Button>();
+        clickAudio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        Debug.Log(this.name + "ButtonTouched");
-        if (button != null && collision.gameObject.tag == "Hand")
+        if (button != null && collision.gameObject.CompareTag("Hand"))
         {
+            clickAudio.Play();
             button.onClick.Invoke();
             return;
         }
-
-        /*
-        if (collision.gameObject.tag == "Hand" && this.name == "Museum")
-        {
-            game.ToMuseum();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "Back To Game")
-        {
-            game.StartGameMountain();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "To Main Menu")
-        {
-            game.ReturnToTitle();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "Start")
-        {
-            game.SelectBiome();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "About")
-        {
-            game.AboutPage();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "AboutBack")
-        {
-            game.MainMenuOpen();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "Mountain Back")
-        {
-            game.MainMenuOpen();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "Mountain")
-        {
-            game.StartGameMountain();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "Desert")
-        {
-            game.StartGameExcavation();
-        }
-        else if (collision.gameObject.tag == "Hand" && this.name == "Quit")
-        {
-            Debug.Log("Application Exited");
-            game.OnQuit();
-        }
-        */
     }
-
 }
