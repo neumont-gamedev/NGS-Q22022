d01a3cb08ded1a2a3178487bf5a15f22ecbebd3b
Alex McDowell <mmcdowell@student.neumont.edu>
2022-05-16 15:13:11 -0600

    adding new system to journal

--- Assets/TPFiles/Scripts/UIManagement/JournalManager.cs
+++ Assets/TPFiles/Scripts/UIManagement/JournalManager.cs
@@ -1,18 +1,91 @@
-using System.Collections.Generic;
 using System.Collections;
-using UnityEngine.UI;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JournalManager : MonoBehaviour
 {
-    [SerializeField] Button[] journalEntryButtons;
+    // Start is called before the first frame update
+    public List<GameObject> journalPages;
+    int page = 0;
 
-    private void Awake()
+    bool IdentifyReady = false;
+
+    //identify pages
+    public GameObject identifyOnPage;
+    public GameObject identifyOffPage;
+
+    public GameObject nextTab;
+    public GameObject backTab;
+    public GameObject identifyTab;
+    public GameObject identifyBackTab;
+
+
+    public void Reset()
     {
-        FossilHolder holder = FindObjectOfType<FossilHolder>();
-        foreach (var jEB in journalEntryButtons)
+        identifyOnPage.SetActive(false);
+        identifyOffPage.SetActive(false);
+        nextTab.SetActive(true);
+        backTab.SetActive(true);
+        identifyTab.SetActive(true);
+
+        foreach (GameObject i in journalPages)
         {
-            jEB.interactable = holder.IsFound(jEB.name);
+            
+            i.SetActive(false);
+        }
+
+
+        page = 0;
+        journalPages[page].SetActive(true);
+    }
+
+    public void TurnPage()
+    {
+
+        if(page + 1 > journalPages.Count)
+        {
+            Reset();
+        }
+        else
+        {
+            page++;
+            journalPages[page].SetActive(true);
+        }
+        
+    }
+
+    public void BackPage()
+    {
+
+        if (page - 1 < 0)
+        {
+            Reset();
+        }
+        else
+        {
+            journalPages[page].SetActive(false);
+            page--;
+            journalPages[page].SetActive(true);
         }
     }
+
+    public void GoToIdentify()
+    {
+        nextTab.SetActive(false);
+        backTab.SetActive(false);
+        identifyTab.SetActive(false);
+        identifyBackTab.SetActive(true);
+
+        if (IdentifyReady)
+        {
+            identifyOnPage.SetActive(true);
+        }
+        else
+        {
+            identifyOffPage.SetActive(true);
+        }
+    }
+
+
+
 }
