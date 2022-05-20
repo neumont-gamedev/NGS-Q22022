f1139f9a7cfc15e76ad254129af90a561d846b87
Matthew Mason <matthewmason7@outlook.com>
2022-05-20 15:40:42 -0600

    optimizing and organization

--- Assets/TPFiles/Scripts/UIManagement/UIManager.cs
+++ Assets/TPFiles/Scripts/UIManagement/UIManager.cs
@@ -14,31 +14,30 @@ public class UIManager : MonoBehaviour
     public GameObject LabButton;
     public GameObject NoFossilsText;
 
-    #region Generic Methods
+    //Activates or Deactivates panel
+    //based on current state
     public void SwitchActivity(GameObject panel)
     {
         if (panel.activeInHierarchy) panel.SetActive(false);
         else panel.SetActive(true);
     }
 
+    //Makes give panel the only activate one
     public void ActivatePanel(GameObject panel)
     {
         DeactivatePanels();
         panel.SetActive(true);
     }
 
+    //Deactivates all panels
     public void DeactivatePanels()
     {
         foreach(var p in panels) p.SetActive(false);
     }
-    #endregion
 
-    #region UI Menus
-
-    // Scene Indices: Museum = 0, Digging = 1, Cleaning = 2
+    //Changes Objective based on fossil count
     public void ObjectiveChange()
     {
-        //load anything that needs to be loaded
         if (FossilHolder.backpack.Count == 0)
         {
             txtObjective.text = "Go find a fossil!";
@@ -49,6 +48,8 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    //checks to see if player can enter lab
+    //based on if they have any fossils to clean
     public bool LoadLab()
     {
         //If player doesnt have a fossil
@@ -70,5 +71,25 @@ public class UIManager : MonoBehaviour
             return true;
         }
     }
-    #endregion
+
+    //Changes current obejctive in digging scene
+    //Based on what step of the process your in
+    public void DiggingObjective(int step)
+    {
+        switch (step) 
+        {
+            case 0:
+                txtObjective.text = "Use map to find a fossil";
+                break;
+            case 1:
+                txtObjective.text = "Completely uncover fossil";
+                break;
+            case 2:
+                txtObjective.text = "Apply Plaster Strip";
+                break;
+            case 3:
+                txtObjective.text = "Return to lab or Find more fossils";
+                break;
+        }
+    }
 }
