1f77fd867474d7ba7eecd867802034bc3b95af37
CameronChildress <cameronchildress4@gmail.com>
2022-05-23 15:37:37 -0600

    Game Fixed (for now)

--- Assets/Scripts/Voxel/VoxelWorld/GameManager.cs
+++ Assets/Scripts/Voxel/VoxelWorld/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     public void Start()
     {
         uiManager = FindObjectOfType<UIManager>();
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneAt(0)) uiManager.ObjectiveChange();
+        if (SceneManager.GetActiveScene() != SceneManager.GetSceneAt(0) && SceneManager.GetActiveScene() != SceneManager.GetSceneAt(4)) uiManager.DiggingObjective(0);
     }
 
     //Build Reference: Museum=0 Dessert=1 Mountains=2 Riverbed=3 Lab=4
@@ -20,7 +22,6 @@ public class GameManager : MonoBehaviour
         {
             case 0:
                 SceneManager.LoadScene(0);
-                uiManager.ObjectiveChange();
                 break;
             case 1:
                 SceneManager.LoadScene(1);
--- Assets/TPFiles/Marching Cubes/Scripts/Fossil.cs
+++ Assets/TPFiles/Marching Cubes/Scripts/Fossil.cs
@@ -7,9 +7,11 @@ public class Fossil : MonoBehaviour
 {
     public bool cleaned = false;
     public AudioClip[] clips;
+    private UIManager uiBoi;
 
     private void Awake()
     {
+        uiBoi = FindObjectOfType<UIManager>();
         ani = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
         shell = transform.Find("Shell").gameObject;
@@ -33,6 +35,7 @@ public class Fossil : MonoBehaviour
                 unburied = true;
                 if (!foundAudioPlayed)
                 {
+                    uiBoi.DiggingObjective(2);
                     foundAudioPlayed = true;
                     audioSource.clip = clips[0];
                     audioSource.Play();
@@ -52,6 +55,7 @@ public class Fossil : MonoBehaviour
             if (!startDigging)
             {
                 startDigging = true;
+                uiBoi.DiggingObjective(1);
             }
             if (!unburied)
             {
@@ -137,6 +141,7 @@ public class Fossil : MonoBehaviour
 
         audioSource.clip = clips[1];
         audioSource.Play();
+        uiBoi.DiggingObjective(3);
         StartCoroutine(PlasterBackpack(2));
     }
 
