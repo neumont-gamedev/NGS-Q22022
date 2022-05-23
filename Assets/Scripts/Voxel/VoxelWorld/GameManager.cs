1e547d543335c6815e7f36e67d5fd2cb5756b310
Matthew Mason <matthewmason7@outlook.com>
2022-05-23 14:53:27 -0600

    aahhhhhh

--- Assets/Scripts/Voxel/VoxelWorld/GameManager.cs
+++ Assets/Scripts/Voxel/VoxelWorld/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     public void Start()
     {
         uiManager = FindObjectOfType<UIManager>();
-        if(SceneManager.GetActiveScene() == SceneManager.GetAllScenes()[0]) uiManager.ObjectiveChange();
+        if(SceneManager.GetActiveScene() == SceneManager.GetSceneAt(0)) uiManager.ObjectiveChange();
     }
 
     //Build Reference: Museum=0 Dessert=1 Mountains=2 Riverbed=3 Lab=4
