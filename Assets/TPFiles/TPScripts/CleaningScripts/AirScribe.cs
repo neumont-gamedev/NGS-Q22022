6349b1402d0362ecdee41384001641e7ce39ce09
Matthew Mason <matthewmason7@outlook.com>
2022-05-25 15:47:08 -0600

    bruh

--- Assets/TPFiles/TPScripts/CleaningScripts/AirScribe.cs
+++ Assets/TPFiles/TPScripts/CleaningScripts/AirScribe.cs
@@ -67,7 +67,7 @@ public class AirScribe : MonoBehaviour
                     {
                         piecesCleaned++;
                         cUIManager.CleanToggleTextChange(piecesCleaned, currentBone.boneParts.Count);
-                        if (piecesCleaned == currentBone.boneParts.Count + 1)
+                        if (piecesCleaned >= currentBone.boneParts.Count)
                         {
                             cUIManager.CleanToggleChange();
                             currentState = CleaningGameState.COMBINE;
