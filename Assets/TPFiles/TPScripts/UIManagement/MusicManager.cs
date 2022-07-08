6c497a967f271f38183e0edc2dc46db3281a66d8
JordanLim-exe <JLim@student.neumont.edu>
2022-07-08 15:08:00 -0600

    Audio Push

---
+++ Assets/TPFiles/TPScripts/UIManagement/MusicManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MusicManager : MonoBehaviour
+{
+    //Warning: Shuffle funcitonality is not implemented, due to not being required at current time, please adjust code if case is needed.
+    private int currentTrack;
+    
+    [SerializeField]
+    public bool PlayMusic;
+    public List<AudioSource> trackList;
+
+
+    public void Start()
+    {
+        if (PlayMusic)
+        {
+            currentTrack = 0;
+            StartCoroutine(PlaySongCo());
+        }
+    }
+
+    IEnumerator PlaySongCo()
+    {
+        //Warning: This Code does not cover the possibility on there being only 1 AudioSource in the trackList, Please adjust code if case is needed.
+        while (PlayMusic)
+        {
+            trackList[currentTrack].Play();
+            yield return new WaitForSeconds(trackList[currentTrack].clip.length);
+            currentTrack++;
+            if(currentTrack >= trackList.Count)
+            {
+                currentTrack = 0;
+            }
+
+            /*if (trackList[currentTrack].isPlaying)
+            {
+                yield return null;
+            }
+            else if(nextTrack > trackList.Count - 1)
+            {
+                print("resetlist");
+                nextTrack = 1;
+                currentTrack = 0;
+                yield return null;
+            }
+            else
+            {
+                print("Iterate");
+                currentTrack = nextTrack;
+                nextTrack++;
+                yield return null;
+            }
+            trackList[currentTrack].Play();
+            yield WaitForSeconds(trackList[currentTrack].clip.length);*/
+        }
+    }
+}
