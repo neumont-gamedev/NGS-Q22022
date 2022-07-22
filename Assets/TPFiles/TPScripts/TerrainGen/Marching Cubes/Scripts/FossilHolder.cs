fac10e88467554cec4607369ed4a00e108bfafb5
Alex McDowell <mmcdowell@student.neumont.edu>
2022-07-22 14:09:46 -0600

    trying to get code to work

--- Assets/TPFiles/TPScripts/TerrainGen/Marching Cubes/Scripts/FossilHolder.cs
+++ Assets/TPFiles/TPScripts/TerrainGen/Marching Cubes/Scripts/FossilHolder.cs
@@ -53,7 +53,7 @@ public class FossilHolder : Singleton<FossilHolder>
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            backpack.Add("Allosaurus_Skull");
+            backpack.Add("Utahraptor_Head");
         }
 
         if (Input.GetKeyDown(KeyCode.M))
